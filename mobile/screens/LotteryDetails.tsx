c844121b201fde1ab5d10277e84d2a72cbdb3fe7
Filip Jarno <filip.jarno@callstack.com>
2026-05-05 16:49:01 +0200

    upgrade expo and update readme

--- mobile/screens/LotteryDetails.tsx
+++ mobile/screens/LotteryDetails.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useEffect } from 'react';
+import React, { ReactElement } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import { Lottery, LotteryDetailsRouteProp } from '../types';
